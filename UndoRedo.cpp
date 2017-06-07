e04402e9fcdc4e67c49f26a06f728284e73d19b9
Felipe Chabatura <felipechabat@gmail.com>
2017-06-06 23:58:12 -0300

    Leitura ok, analise do log em andamento

--- UndoRedo.cpp
+++ UndoRedo.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -11,15 +14,17 @@ typedef unsigned long long ull;
 
 #define MAX 200
 
-map <string, string> banco;
-map <string, bool> comitted;
-
 typedef struct a{
-  char nome[MAX];
-  char atributo[MAX];
-  double vInit;
-  double vEnd;
-} rola;
+  string nome;
+  string atributo;
+  string velho;
+  string novo;
+} transaction_t;
+
+map <string, string> banco;
+map <string, bool> committed;
+stack <transaction_t> redo;
+queue <transaction_t> undo;
 
 void carregarBanco();
 void printarBanco();
@@ -27,8 +32,8 @@ void analisarLog();
 
 int main(void){
   carregarBanco();
-  printarBanco();
-  //analisarLog();
+  //printarBanco();
+  analisarLog();
   return 0;
 }
 
@@ -49,12 +54,13 @@ void carregarBanco(){
       atributo[i] = c;
       c = fgetc(f);
     }
+    atributo[i] = '\0';
     for(i = 0; c != '\n' && c != EOF; i++){                       // Enquanto ele nao chegar na nova linha
       c = fgetc(f);                                               // ou acabar o arquivo
       valor[i] = c;
     }
     valor[i] = '\0';
-    if(c != EOF) banco[atributo] = valor;  // Se não for EOF, joga no map.
+    if(c != EOF) banco[atributo] = valor;                         // Se não for EOF, joga no map.
   }
   fclose(f);                                                      // Fecha o arquivo
   return;
@@ -62,44 +68,44 @@ void carregarBanco(){
 
 void printarBanco(){                                              // itera no map e printa ele
 
-
-  for(map<string, int>::iterator i = banco.begin(); i != banco.end(); i++){
-    printf("%s = %d\n", i->first.c_str(), i->second);
+  printf("\n");
+  for(map<string, string>::iterator i = banco.begin(); i != banco.end(); i++){
+    printf("%s = %s\n", i->first.c_str(), i->second.c_str());
   }
   printf("\n");
 }
 
-/*
-
 void analisarLog(){
   FILE *f;
-  char c, s[MAX], junk[MAX];
-  ull i, size;
+  char c, s[MAX], junk[MAX], tmpname[MAX];
+  ull i, j, k, size;
   bool CkptFound = false;
   f = fopen("log.txt", "r");
   fseek(f, 0, SEEK_END);
   size = ftell(f);                                              // salva o valor do tamanho do arquivo
-  for(i = 1; i <= size; i++){                                   // Vai iterando até chegar no começo do arquivo
+  for(i = 2; i <= size; i++){                                   // Vai iterando até chegar no começo do arquivo
     fseek(f, size-i, SEEK_SET);
-    if((c = fgetchar(f)) == '\n'){                              // Se é /n, é pq iniciou uma nova operação
+    if((c = fgetc(f)) == '\n'){                                 // Se é /n, é pq iniciou uma nova operação
       fgets(s, MAX, f);                                         // Pega essa operação.
       strcpy(junk, s);                                          // Copia para uma variavel que possa ser zoada
       junk[strlen(junk)-1] = '\0';                              // Coloca o /0 nela
       if(!strcmp(s, "<END CKPT>")) CkptFound = true;            // Se a operação for END_CKPT...
-      junk[11] = '\0';
-      else if(!strcmp(s, "<Start CKPT")){                       // Se a operação for start checkpoint...
+      else if(junk[11] = '\0', !strcmp(junk, "<Start CKPT")){      // Se a operação for start checkpoint...
 
       }
-      junk[7] = '\0';
-      else if(!strcmp(s, "<Commit")){                           // Se for um commit...
-
+      else if(junk[7] = '\0', !strcmp(junk, "<Commit")){           // Se for um commit...
+        for(j = 8, k = 0; s[j] != '>' ;j++){
+          tmpname[k++] = s[j];
+        }
+        tmpname[k] = '\0';
+        committed[tmpname] = true;
+        printf("%s\n", tmpname);
       }
-      junk[6] = '\0';
-      else if(!strcmp(s, "<start")){                            // Se for start transação....
+      else if(junk[6] = '\0', !strcmp(junk, "<start")){            // Se for start transação....
       }
       else{                                                     // Se for uma operação de modificação no banco...
+        
       }
     }
   }
 }
-*/
